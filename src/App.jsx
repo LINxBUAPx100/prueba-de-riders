9957188a374b36751babcec59aaf0009157f8a7f
LINTAREAS <ts202534004@alm.buap.mx>
2026-04-27 17:17:28 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function HomeView({ nav }) {
           ) : (
     <iframe 
       style={{ position: "absolute", inset: 0, width: "100%", height: "100%", border: "none" }}
-      src="https://player.vimeo.com/video/685995963?autoplay=1&title=0&byline=0&portrait=0" 
+      src="https://player.vimeo.com/video/201106279?autoplay=1&title=0&byline=0&portrait=0" 
       allow="autoplay; fullscreen; picture-in-picture" 
       allowFullScreen
       title="Showreel Riders.Media"
